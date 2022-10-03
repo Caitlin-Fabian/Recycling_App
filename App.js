6927c975d5407e2a9249b79be1701f729adb8f30
Caitlin <caitlinfstar@gmail.com>
2022-10-03 19:51:46 -0400

    Login Screen and working on Navigation

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/LoginScreen';
+import HomeScreen from './src/HomeScreen';
+
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+        headerShown: false,
+        gestureEnabled: false
+      }}
+      >
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{ title: 'Welcome' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/LoginScreen.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useCallback, useState } from 'react';
+import { Text,View,Image,StyleSheet, TextInput, Alert,Button } from "react-native";
+import { useFonts} from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+
+const LoginScreen = () => {
+  const [text, setText] = useState('');
+
+
+  const [fontsLoaded] = useFonts({
+    'FredokaOne': require('../assets/fonts/FredokaOne-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+
+    return(
+        <View style={styles.container} onLayout={onLayoutRootView}>
+            <Image source={require('../assets/logo.png')} style={styles.logo}/>
+            <View><Text style={{fontFamily: "FredokaOne", fontSize: 60}}> Sign in</Text>
+              <Text style={styles.text}>Email:</Text>
+              <TextInput
+                style={styles.contentInput}
+                placeholder="email@address.com"
+                onChangeText={newText => setText(newText)}
+                defaultValue ={text}
+              />  
+              <Text style={styles.text}>Password:</Text>
+                 <TextInput
+                style={styles.contentInput}
+                placeholder="password"
+                onChangeText={newText => setText(newText)}
+                defaultValue ={text}
+              />
+            </View>
+            <Button
+              title= "Sumbit" 
+              onPress={()=> Alert.alert('Will fix later')}
+            />
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#F0F3BD',
+        alignItems:'center'
+    },
+    text:{
+      marginTop: 10,
+      fontFamily: "FredokaOne",
+      fontSize:30,
+      textAlign: 'justify',
+    },  
+    logo:{
+        marginTop: 70,
+        width: 200,
+        height: 200,
+        alignItems: 'center',
+    },
+    contentInput:{
+      height: 40,
+      width: 200,
+      backgroundColor: '#FFF',
+    }
+
+})
+
+export default LoginScreen;

e5ab5c28f8d118525a9f6ac2e19990849931426e
Jacki Z <jzamitalo@icloud.com>
2022-10-12 22:15:09 -0400

    Add basic import requirements to homescreen build page

---
+++ src/HomeScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { useCallback, useState } from 'react';
+import { Text,View,Image,StyleSheet, TextInput, Alert,Button } from "react-native";
+import { useFonts} from "expo-font";
+import * as SplashScreen from 'expo-splash-screen';
+import Realm from "realm";
+
+

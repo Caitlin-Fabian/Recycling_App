92c5ebbe92373b342f75862249b390b95d342877
Jacki Z <jzamitalo@icloud.com>
2022-10-17 14:42:03 -0400

    Add basic styling and content to home page

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -3,14 +3,42 @@ import { useCallback, useState } from 'react';
 import { Text,View,Image,StyleSheet, TextInput, Alert,Button } from "react-native";
 import { useFonts} from "expo-font";
 import * as SplashScreen from 'expo-splash-screen';
+import { AutourOne_400Regular } from '@expo-google-fonts/dev';
 
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
     return(
-        <View>
-            <Text>This will be a beautiful home screen one day!</Text>
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.text}>The progress bar will be at the top!</Text>
+            </View>
+            <View>
+                <Text style={styles.text}>The articles will be here (in the middle)!</Text>
+            </View>
+            <View>
+                <Text style={styles.text}>Navigation bar will go at the bottom.</Text>
+                <Button 
+                    title='Go back to log in page (will delete later' 
+                    onPress={() => navigation.navigate('LoginScreen')} 
+                />
+            </View>
         </View>
+
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: '#F0F3BD',
+        flexDirection:'column',
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    text:{
+      fontFamily: "FredokaOne",
+      fontSize:30
+    }
+})
 
 
 export default HomeScreen;

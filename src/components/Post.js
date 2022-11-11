2ff0e162f4079601b28fbdbd43743d7c8788f154
Zackary Kiener <64226837+ZackFred07@users.noreply.github.com>
2022-11-10 19:19:33 -0500

    Feed Screen-No Image, Now less dumpster pile

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,57 +1,58 @@
 import React, { Component } from "react";
 import { StyleSheet, Image, Text, View, ImageBackground } from "react-native";
 
-export default class Post extends Component {
-	render(props) {
-		return (
-			<View style={styles.Post}>
-				<View style={styles.Group534}>
-					<View style={styles.User}>
-						<Image
-							style={styles.profile}
-							source={{
-								uri: "https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1pe92a9a0gm-542%3A9?alt=media&token=baab46c0-ed66-4109-901a-650bb4360ece",
-							}}
-						/>
-						<Text style={styles.Txt242}>props.username</Text>
-					</View>
+export default function Post({ username }) {
+	return (
+		<View style={styles.Post}>
+			<View style={styles.Group534}>
+				<View style={styles.User}>
 					<Image
-						style={styles.Image}
+						style={styles.profile}
 						source={{
-							uri: props.imagesrc,
+							uri: "https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1pe92a9a0gm-542%3A9?alt=media&token=baab46c0-ed66-4109-901a-650bb4360ece",
 						}}
 					/>
-					<View style={styles.BommomPart}>
-						<View style={styles.Comment}>
-							<Text style={styles.Txt399}>Comment...</Text>
-						</View>
-						<Image
-							style={styles.IconHeartOutline}
-							source={{
-								uri: "https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1pe92a9a0gm-447%3A11?alt=media&token=a3ff24cc-c2ef-4f18-b1c6-0699b5a81296",
-							}}
-						/>
+					<Text style={styles.Txt242}>{username}</Text>
+				</View>
+				<Image
+					style={styles.Image}
+					source={
+						{
+							// uri: props.imagesrc,
+						}
+					}
+				/>
+				<View style={styles.BommomPart}>
+					<View style={styles.Comment}>
+						<Text style={styles.Txt399}>Comment...</Text>
 					</View>
+					<Image
+						style={styles.IconHeartOutline}
+						source={{
+							uri: "https://firebasestorage.googleapis.com/v0/b/unify-bc2ad.appspot.com/o/1pe92a9a0gm-447%3A11?alt=media&token=a3ff24cc-c2ef-4f18-b1c6-0699b5a81296",
+						}}
+					/>
 				</View>
 			</View>
-		);
-	}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
 	Post: {
 		display: "flex",
 		flexDirection: "column",
-		justifyContent: "flex-start",
-		alignItems: "flex-start",
+		justifyContent: "center",
+		alignItems: "center",
 		paddingTop: 4,
 		paddingBottom: 8,
 		paddingLeft: 4,
 		paddingRight: 4,
 		borderRadius: 20,
 		backgroundColor: "rgba(0,168,150,1)",
-		width: 370,
-		height: 237,
+		minWidth: 370,
+		minHeight: 237,
+		margin: 5,
 	},
 	Group534: {
 		display: "flex",
@@ -75,7 +76,7 @@ const styles = StyleSheet.create({
 	},
 	Txt242: {
 		fontSize: 19,
-		fontFamily: "Inter, sans-serif",
+		fontFamily: "Fredoka One",
 		fontWeight: "700",
 		color: "rgba(0,0,0,1)",
 		width: 273,
@@ -84,14 +85,17 @@ const styles = StyleSheet.create({
 
 	Image: {
 		width: 360,
-		height: 166,
+		maxHeight: 500,
 		marginBottom: 3,
+		backgroundColor: "#000000",
+		borderRadius: 10,
+		height: 200,
 	},
 	BommomPart: {
 		display: "flex",
 		flexDirection: "row",
-		justifyContent: "flex-start",
-		alignItems: "flex-start",
+		justifyContent: "center",
+		alignItems: "center",
 		paddingTop: 0,
 		paddingBottom: 0,
 		paddingLeft: 0,
@@ -100,8 +104,8 @@ const styles = StyleSheet.create({
 	Comment: {
 		display: "flex",
 		flexDirection: "column",
-		justifyContent: "flex-start",
-		alignItems: "flex-start",
+		justifyContent: "center",
+		alignItems: "center",
 		paddingTop: 0,
 		paddingBottom: 0,
 		paddingLeft: 7,
@@ -112,7 +116,7 @@ const styles = StyleSheet.create({
 	},
 	Txt399: {
 		fontSize: 12,
-		fontFamily: "Inter, sans-serif",
+		fontFamily: "Fredoka One",
 		fontWeight: "400",
 		color: "rgba(0,0,0,1)",
 		width: 195,
--- src/screens/FeedScreen.js
+++ src/screens/FeedScreen.js
@@ -2,41 +2,45 @@ import { Text, View, StyleSheet } from "react-native";
 import React, { Component } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import Post from "../components/Post";
+import { ScrollView } from "react-native-gesture-handler";
 
-export default class FeedScreen extends Component {
-	render() {
-		const data = [
-			{
-				username: "F",
-				imagesrc:
-					"https://tg-cdn.azureedge.net/sites/default/files/images/paragraph/italrb/easy_guide_grass.jpg",
-			},
-			{
-				username: "D",
-				imagesrc:
-					"https://upload.wikimedia.org/wikipedia/commons/3/32/Empty_beer_bottles_on_grass.jpg",
-			},
-		];
+const FeedScreen = () => {
+	const data = [
+		{
+			username: "F",
+			imagesrc:
+				"https://tg-cdn.azureedge.net/sites/default/files/images/paragraph/italrb/easy_guide_grass.jpg",
+		},
+		{
+			username: "D",
+			imagesrc:
+				"https://upload.wikimedia.org/wikipedia/commons/3/32/Empty_beer_bottles_on_grass.jpg",
+		},
+		{
+			username: "Zack",
+			imagesrc:
+				"https://upload.wikimedia.org/wikipedia/commons/3/32/Empty_beer_bottles_on_grass.jpg",
+		},
+	];
 
-		return (
-			<View>
-				<Text>FeedScreen</Text>
-				<View>
-					{data.map((data) => {
-						return <Post props={data}></Post>;
-					})}
-				</View>
+	return (
+		<ScrollView>
+			<Text>FeedScreen</Text>
+			<View style={styles.container}>
+				{data.map((data) => {
+					return <Post username={data.username}></Post>;
+				})}
 			</View>
-		);
-	}
-}
+		</ScrollView>
+	);
+};
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 		backgroundColor: "#F0F3BD",
 		flexDirection: "column",
-		justifyContent: "center",
+		justifyContent: "flex-start",
 		alignItems: "center",
 	},
 	text: {
@@ -44,3 +48,5 @@ const styles = StyleSheet.create({
 		fontSize: 30,
 	},
 });
+
+export default FeedScreen;

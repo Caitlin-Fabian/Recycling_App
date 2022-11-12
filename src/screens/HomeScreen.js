e55eceee59b00af16eeae50a78707de794e61459
JackiZam <jzamitalo@icloud.com>
2022-11-12 00:41:38 -0500

    Link homepage articles to external websites

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,44 +1,42 @@
 import React from "react";
-import { Text, View, Image, StyleSheet, ScrollView, TextInput, Alert, Button } from "react-native";
+import { Text, View, Image, StyleSheet, ScrollView, TextInput, Alert, Button, Linking } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import Icon from "react-native-vector-icons/FontAwesome";
-import Article from "../components/Articles";
 
 const HomeScreen = () => {
-  const data = [
-    {
-      // need to find 5 articles and photos that correspond to them
-      description: "This article from Stanford University answers multiple questions about recycling, why it's important, and it's impact.",
-      link: "https://lbre.stanford.edu/pssistanford-recycling/frequently-asked-questions/frequently-asked-questions-benefits-recycling",
-    },
-    {
-      description: "Learn about easy ways to recycle everyday items around you with this article.",
-      link: "https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables",
-    },
-    {
-      description: "Explore the meanings of the different numbers on recyclable plastic items!",
-      link: "https://www.ebpsupply.com/blog/plastic-recycling-codes-types-explanation",
-    },
-    {
-      description: "This article explains the positive impacts that recycling has on the environment.",
-      link: "https://biofriendlyplanet.com/nature/environment/the-positive-effect-of-recycling-on-the-environment/",
-    },
-    {
-      description: "Read about unique ideas for recycling various objects!",
-      link: "https://sassytownhouseliving.com/16-creative-ideas-recycling-common-household-items/",
-    }
-  ];
-
-  const image1 = "../pictures/pexels-photo-356079.jpeg";
-
   return (
     <SafeAreaProvider>
       <View style={styles.container}>
         <ScrollView>
-            {data.map((data) => {
-              return <Article description = {data.description} link = {data.link}></Article>;
-            })}
-          </ScrollView>
+          <View style={styles.Article}>
+            <Text style={styles.Text}>This article from Stanford University answers multiple questions about recycling, why it's important, and it's impact.</Text>
+              <Text style={styles.LinkRight}
+                onPress={() => Linking.openURL("https://lbre.stanford.edu/pssistanford-recycling/frequently-asked-questions/frequently-asked-questions-benefits-recycling")}>Read the article here!</Text>
+          </View>
+
+          <View style={styles.Article}>
+            <Text style={styles.Text}>Learn about easy ways to recycle everyday items around you with this article.</Text>
+              <Text style={styles.LinkRight}
+                onPress={() => Linking.openURL("https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables")}>Read the article here!</Text>
+          </View>
+
+          <View style={styles.Article}>
+            <Text style={styles.Text}>Explore the meanings of the different numbers on recyclable plastic items!</Text>
+              <Text style={styles.LinkRight}
+                onPress={() => Linking.openURL("https://www.ebpsupply.com/blog/plastic-recycling-codes-types-explanation")}>Read the article here!</Text>
+          </View>
+
+          <View style={styles.Article}>
+            <Text style={styles.Text}>This article explains the positive impacts that recycling has on the environment.</Text>
+              <Text style={styles.LinkRight}
+                onPress={() => Linking.openURL("https://biofriendlyplanet.com/nature/environment/the-positive-effect-of-recycling-on-the-environment/")}>Read the article here!</Text>
+          </View>
+
+          <View style={styles.Article}>
+            <Text style={styles.Text}>Read about unique ideas for recycling various objects!</Text>
+              <Text style={styles.LinkRight}
+                onPress={() => Linking.openURL("https://sassytownhouseliving.com/16-creative-ideas-recycling-common-household-items/")}>Read the article here!</Text>
+          </View>
+        </ScrollView>
       </View>
     </SafeAreaProvider>
   );
@@ -56,6 +54,24 @@ const styles = StyleSheet.create({
     fontFamily: "Fredoka One",
     fontSize: 30,
   },
+  Article: {
+      margin: 15,
+  },
+  Text: {
+      fontFamily: "Fredoka One",
+      fontSize: 20,
+      paddingBottom: 8,
+  },
+  Link: {
+      marginLeft: 5,
+  },
+  LinkLeft: {
+      fontSize: 15,
+      paddingBottom: 3,
+      textDecorationLine: "underline",
+  },
+  LinkRight: {
+  }
 });
 
 export default HomeScreen;

59d18e02616b604a5afaecb5e99cafa53ff5bae2
JackiZam <jzamitalo@icloud.com>
2022-11-13 12:04:08 -0500

    Format articles on home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -8,32 +8,54 @@ const HomeScreen = () => {
       <View style={styles.container}>
         <ScrollView>
           <View style={styles.Article}>
-            <Text style={styles.Text}>This article from Stanford University answers multiple questions about recycling, why it's important, and it's impact.</Text>
-              <Text style={styles.LinkRight}
-                onPress={() => Linking.openURL("https://lbre.stanford.edu/pssistanford-recycling/frequently-asked-questions/frequently-asked-questions-benefits-recycling")}>Read the article here!</Text>
+            <View style={styles.Hook}>
+              <Image source={require("../assets/pictures/question.jpeg")} style={styles.Picture}/>
+              <Text style={styles.Text}>This article from Stanford University answers multiple questions about recycling, why it's important, and it's impact.</Text>
+            </View>
+            <Text style={styles.Link}
+              onPress={() => Linking.openURL("https://lbre.stanford.edu/pssistanford-recycling/frequently-asked-questions/frequently-asked-questions-benefits-recycling")}>Read the article here!</Text>
           </View>
 
           <View style={styles.Article}>
-            <Text style={styles.Text}>Learn about easy ways to recycle everyday items around you with this article.</Text>
-              <Text style={styles.LinkRight}
-                onPress={() => Linking.openURL("https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables")}>Read the article here!</Text>
+            <View style={styles.Hook}>
+              <Image source={require("../assets/pictures/bin.webp")} style={styles.Picture}/>
+              <Text style={styles.Text}>Learn about easy ways to recycle everyday items around you with this article.</Text>
+            </View>
+            <Text style={styles.Link}
+              onPress={() => Linking.openURL("https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables")}>Read the article here!</Text>
           </View>
 
           <View style={styles.Article}>
-            <Text style={styles.Text}>Explore the meanings of the different numbers on recyclable plastic items!</Text>
-              <Text style={styles.LinkRight}
-                onPress={() => Linking.openURL("https://www.ebpsupply.com/blog/plastic-recycling-codes-types-explanation")}>Read the article here!</Text>
+            <View style={styles.Hook}>
+              <Image source={require("../assets/pictures/plastic.webp")} style={styles.Picture}/>
+              <Text style={styles.Text}>Explore the meanings of the different numbers on recyclable plastic items!</Text>
+            </View>
+            <Text style={styles.Link}
+              onPress={() => Linking.openURL("https://www.ebpsupply.com/blog/plastic-recycling-codes-types-explanation")}>Read the article here!</Text>
           </View>
 
           <View style={styles.Article}>
-            <Text style={styles.Text}>This article explains the positive impacts that recycling has on the environment.</Text>
-              <Text style={styles.LinkRight}
-                onPress={() => Linking.openURL("https://biofriendlyplanet.com/nature/environment/the-positive-effect-of-recycling-on-the-environment/")}>Read the article here!</Text>
+            <View style={styles.Hook}>
+              <Image source={require("../assets/pictures/environment.jpeg")} style={styles.Picture}/>
+              <Text style={styles.Text}>This article explains the positive impacts that recycling has on the environment.</Text>
+            </View>
+            <Text style={styles.Link}
+              onPress={() => Linking.openURL("https://biofriendlyplanet.com/nature/environment/the-positive-effect-of-recycling-on-the-environment/")}>Read the article here!</Text>
           </View>
 
           <View style={styles.Article}>
+            <View style={styles.Hook}>
+              <Image source={require("../assets/pictures/idea.webp")} style={styles.Picture}/>
+              <Text style={styles.Text}>Read about unique ideas for recycling various objects!</Text>
+            </View>
+            <Text style={styles.Link}
+              onPress={() => Linking.openURL("https://sassytownhouseliving.com/16-creative-ideas-recycling-common-household-items/")}>Read the article here!</Text>
+          </View>
+
+          <View style={styles.Article}>
+            <Image source={require("../assets/pictures/idea.webp")} style={styles.Picture}/>
             <Text style={styles.Text}>Read about unique ideas for recycling various objects!</Text>
-              <Text style={styles.LinkRight}
+              <Text style={styles.Link}
                 onPress={() => Linking.openURL("https://sassytownhouseliving.com/16-creative-ideas-recycling-common-household-items/")}>Read the article here!</Text>
           </View>
         </ScrollView>
@@ -47,31 +69,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#F0F3BD",
     flexDirection: "column",
-    justifyContent: "center",
     alignItems: "center",
   },
-  text: {
-    fontFamily: "Fredoka One",
-    fontSize: 30,
-  },
   Article: {
-      margin: 15,
+    display: "flex",
+    flexDirection: "column",
+    margin: 15,
+  },
+  Hook: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  Picture: {
+    height: 100,
+    width: 100,
+    paddingRight: 10,
   },
   Text: {
+      flex: 1,
       fontFamily: "Fredoka One",
-      fontSize: 20,
+      fontSize: 15,
       paddingBottom: 8,
+      paddingLeft: 20,
   },
   Link: {
-      marginLeft: 5,
+      paddingBottom: 10,
   },
-  LinkLeft: {
-      fontSize: 15,
-      paddingBottom: 3,
-      textDecorationLine: "underline",
-  },
-  LinkRight: {
-  }
 });
 
 export default HomeScreen;

5f27a6299b98e9a4c3350d69eb124876eb39f41d
JackiZam <jzamitalo@icloud.com>
2022-11-14 02:29:05 -0500

    Edit formatting for better presentation of home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,57 +7,51 @@ const HomeScreen = () => {
     <SafeAreaProvider>
       <View style={styles.container}>
         <ScrollView>
-          <View style={styles.Article}>
-            <View style={styles.Hook}>
-              <Image source={require("../assets/pictures/question.jpeg")} style={styles.Picture}/>
+          <View style={styles.WholeThing}>
+            <Image source={require("../assets/pictures/question.jpeg")} style={styles.Picture}/>
+            <View style={styles.Article}>
               <Text style={styles.Text}>This article from Stanford University answers multiple questions about recycling, why it's important, and it's impact.</Text>
+              <Text style={styles.Link}
+                onPress={() => Linking.openURL("https://lbre.stanford.edu/pssistanford-recycling/frequently-asked-questions/frequently-asked-questions-benefits-recycling")}>Read the article here!</Text>
             </View>
-            <Text style={styles.Link}
-              onPress={() => Linking.openURL("https://lbre.stanford.edu/pssistanford-recycling/frequently-asked-questions/frequently-asked-questions-benefits-recycling")}>Read the article here!</Text>
           </View>
 
-          <View style={styles.Article}>
-            <View style={styles.Hook}>
-              <Image source={require("../assets/pictures/bin.webp")} style={styles.Picture}/>
+          <View style={styles.WholeThing}>
+            <Image source={require("../assets/pictures/bin.webp")} style={styles.Picture}/>
+            <View style={styles.Article}>
               <Text style={styles.Text}>Learn about easy ways to recycle everyday items around you with this article.</Text>
+              <Text style={styles.Link}
+                onPress={() => Linking.openURL("https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables")}>Read the article here!</Text>
             </View>
-            <Text style={styles.Link}
-              onPress={() => Linking.openURL("https://www.epa.gov/recycle/how-do-i-recycle-common-recyclables")}>Read the article here!</Text>
           </View>
 
-          <View style={styles.Article}>
-            <View style={styles.Hook}>
-              <Image source={require("../assets/pictures/plastic.webp")} style={styles.Picture}/>
+          <View style={styles.WholeThing}>
+            <Image source={require("../assets/pictures/plastic.webp")} style={styles.Picture}/>
+            <View style={styles.Article}>
               <Text style={styles.Text}>Explore the meanings of the different numbers on recyclable plastic items!</Text>
+              <Text style={styles.Link}
+                onPress={() => Linking.openURL("https://www.ebpsupply.com/blog/plastic-recycling-codes-types-explanation")}>Read the article here!</Text>
             </View>
-            <Text style={styles.Link}
-              onPress={() => Linking.openURL("https://www.ebpsupply.com/blog/plastic-recycling-codes-types-explanation")}>Read the article here!</Text>
           </View>
 
-          <View style={styles.Article}>
-            <View style={styles.Hook}>
-              <Image source={require("../assets/pictures/environment.jpeg")} style={styles.Picture}/>
+          <View style={styles.WholeThing}>
+            <Image source={require("../assets/pictures/environment.jpeg")} style={styles.Picture}/>
+            <View style={styles.Article}>
               <Text style={styles.Text}>This article explains the positive impacts that recycling has on the environment.</Text>
+              <Text style={styles.Link}
+                onPress={() => Linking.openURL("https://biofriendlyplanet.com/nature/environment/the-positive-effect-of-recycling-on-the-environment/")}>Read the article here!</Text>
             </View>
-            <Text style={styles.Link}
-              onPress={() => Linking.openURL("https://biofriendlyplanet.com/nature/environment/the-positive-effect-of-recycling-on-the-environment/")}>Read the article here!</Text>
           </View>
 
-          <View style={styles.Article}>
-            <View style={styles.Hook}>
-              <Image source={require("../assets/pictures/idea.webp")} style={styles.Picture}/>
-              <Text style={styles.Text}>Read about unique ideas for recycling various objects!</Text>
-            </View>
-            <Text style={styles.Link}
-              onPress={() => Linking.openURL("https://sassytownhouseliving.com/16-creative-ideas-recycling-common-household-items/")}>Read the article here!</Text>
-          </View>
-
-          <View style={styles.Article}>
+          <View style={styles.WholeThing}>
             <Image source={require("../assets/pictures/idea.webp")} style={styles.Picture}/>
-            <Text style={styles.Text}>Read about unique ideas for recycling various objects!</Text>
+            <View style={styles.Article}>
+              <Text style={styles.Text}>Read about unique ideas for recycling various objects!</Text>
               <Text style={styles.Link}
                 onPress={() => Linking.openURL("https://sassytownhouseliving.com/16-creative-ideas-recycling-common-household-items/")}>Read the article here!</Text>
+            </View>
           </View>
+
         </ScrollView>
       </View>
     </SafeAreaProvider>
@@ -70,31 +64,31 @@ const styles = StyleSheet.create({
     backgroundColor: "#F0F3BD",
     flexDirection: "column",
     alignItems: "center",
+    padding: 15,
   },
-  Article: {
-    display: "flex",
-    flexDirection: "column",
-    margin: 15,
-  },
-  Hook: {
-    display: "flex",
+  WholeThing: {
     flexDirection: "row",
+    justifyContent: "space-around",
+    paddingBottom: 25,
+    paddingTop: 8,
   },
   Picture: {
     height: 100,
     width: 100,
-    paddingRight: 10,
+  },
+  Article: {
+    flexDirection: "column",
+    paddingLeft: 5,
+    flexShrink: 1,
   },
   Text: {
       flex: 1,
       fontFamily: "Fredoka One",
       fontSize: 15,
-      paddingBottom: 8,
-      paddingLeft: 20,
   },
   Link: {
-      paddingBottom: 10,
+      paddingTop: 10,
   },
-});
+}); 
 
 export default HomeScreen;
